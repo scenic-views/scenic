2beca2efc5079ed934d229c8ae1d5707ed3c8766
Caleb Thompson <caleb@calebthompson.io>
2014-05-02 15:42:18 -0500

    Teach migrations to drop_view

--- spec/scenic/active_record/schema_spec.rb
+++ spec/scenic/active_record/schema_spec.rb
@@ -7,24 +7,37 @@ end
 describe 'Scenic::ActiveRecord::Schema' do
   describe 'create_view' do
     it 'creates a view from a file' do
-      define_view :views, 1, "SELECT text 'Hello World' AS hello"
+      with_view_definition :views, 1, "SELECT text 'Hello World' AS hello" do
+        View.connection.create_view :views, 1
 
-      View.connection.create_view :views, 1
-
-      expect(View.all.pluck(:hello)).to eq ['Hello World']
-
-      remove_generated_files
+        expect(View.all.pluck(:hello)).to eq ['Hello World']
+      end
     end
   end
 
-  def define_view(name, version, schema)
-    @to_be_removed ||= []
-    view_file = ::Rails.root.join('db', 'views', "#{name}_v#{version}.sql")
-    @to_be_removed << view_file
-    File.open(view_file, 'w') { |f| f.write(schema) }
+  describe 'drop_view' do
+    it 'removes a view from the database' do
+      with_view_definition :things, 1, "SELECT text 'Hi' AS greeting" do
+        View.connection.create_view :things, 1
+
+        View.connection.drop_view :things
+
+        expect(views).not_to include 'things'
+      end
+    end
   end
 
-  def remove_generated_files
-    @to_be_removed.each { |f| File.delete(f) }
+  def views
+    ActiveRecord::Base.connection
+      .execute('SELECT table_name FROM INFORMATION_SCHEMA.views')
+      .map(&:values).flatten
+  end
+
+  def with_view_definition(name, version, schema)
+    view_file = ::Rails.root.join('db', 'views', "#{name}_v#{version}.sql")
+    File.open(view_file, 'w') { |f| f.write(schema) }
+    yield
+  ensure
+    File.delete view_file
   end
 end
