70c6a8c9c23fbb1de1cbcd033d45c6d7c3fdcd78
Caleb Thompson <caleb@calebthompson.io>
2014-07-25 11:18:58 -0500

    Make create_view and drop_view reversible

--- lib/scenic.rb
+++ lib/scenic.rb
@@ -2,6 +2,7 @@ require "scenic/version"
 require "scenic/engine"
 require "scenic/active_record/statements"
 require "scenic/active_record/schema_dumper"
+require "scenic/active_record/command_recorder"
 
 module Scenic
 end
