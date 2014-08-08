b60644bc9fb1e0155601ecbc061e41d1af963478
Derek Prior <derekprior@gmail.com>
2014-08-08 17:27:31 -0400

    Add --sql option to view generator

--- spec/generators/scenic/view/view_generator_spec.rb
+++ spec/generators/scenic/view/view_generator_spec.rb
@@ -12,6 +12,14 @@ describe Scenic::Generators::ViewGenerator, :generator do
     expect(view_definition).to exist
   end
 
+  it "accepts a SQL option to put in the view definition file" do
+    sql = "SELECT foo FROM bar"
+    view_definition = file("db/views/searches_v01.sql")
+    run_generator ["search", "--sql #{sql}"]
+
+    expect(view_definition).to contain(sql)
+  end
+
   it "updates an existing view" do
     migration = file("db/migrate/update_searches_to_version_2.rb")
     view_definition = file("db/views/searches_v02.sql")
