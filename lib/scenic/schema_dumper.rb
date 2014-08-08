1678896c2c6255653f8cf382af975b0e8c9c1c49
Caleb Thompson <caleb@calebthompson.io>
2014-08-08 13:52:18 -0500

    This could break everything, frankly

--- lib/scenic/schema_dumper.rb
+++ lib/scenic/schema_dumper.rb
@@ -23,15 +23,16 @@ module Scenic
 
     private
 
-    # This method will be present in Rails 4.1.5 or 5.0.0 and can be removed
-    # then.
-    def ignored?(table_name)
-      ["schema_migrations", ignore_tables].flatten.any? do |ignored|
-        case ignored
-        when String; remove_prefix_and_suffix(table_name) == ignored
-        when Regexp; remove_prefix_and_suffix(table_name) =~ ignored
-        else
-          raise StandardError, "ActiveRecord::SchemaDumper.ignore_tables accepts an array of String and / or Regexp values."
+    unless ActiveRecord::SchemaDumper.instance_methods(false).include?(:ignored?)
+      # This method will be present in Rails 4.2.0 and can be removed then.
+      def ignored?(table_name)
+        ["schema_migrations", ignore_tables].flatten.any? do |ignored|
+          case ignored
+          when String; remove_prefix_and_suffix(table_name) == ignored
+          when Regexp; remove_prefix_and_suffix(table_name) =~ ignored
+          else
+            raise StandardError, "ActiveRecord::SchemaDumper.ignore_tables accepts an array of String and / or Regexp values."
+          end
         end
       end
     end
