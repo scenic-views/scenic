997f634ea908b9da7ef389463c6a51ca5bdbd7b1
Derek Prior <derekprior@gmail.com>
2015-11-13 19:30:33 -0500

    Extract an abstract database adapter
    `Scenic::Adapters::Abstract` lays out the interface for Scenic database
    adapters. It contains template methods for all methods required of a
    Scenic adapter with actual implementations for `create_view` and
    `drop_view` which are identical for basic use cases across all
    relational databases I've encountered.
    
    Using the abstract adapter as a base, for instance, a hypothetical MySQL
    adapter would need only to implement the `views` method that retrieves
    all views and their associated schema for dumping to the `schema.rb`
    file.
    
    As part of this extraction, I converted to using instances for the
    adapters rather than class methods. This feels better and wasn't a good
    deal of work. I'd prefer instance based APIs rather than class-based
    APIs.

--- lib/scenic/adapters/postgres.rb
+++ lib/scenic/adapters/postgres.rb
@@ -1,7 +1,15 @@
+require "scenic/adapters/abstract"
+
 module Scenic
   module Adapters
-    module Postgres
-      def self.views
+    class Postgres < Abstract
+      # Returns an array of views in the database.
+      #
+      # This collection of views is used by the [Scenic::SchemaDumper] to
+      # populate the `schema.rb` file.
+      #
+      # @return [Array<Scenic::View>]
+      def views
         execute(<<-SQL).map { |result| Scenic::View.new(result) }
           SELECT viewname, definition, FALSE AS materialized
           FROM pg_views
@@ -15,30 +23,29 @@ module Scenic
         SQL
       end
 
-      def self.create_view(name, sql_definition)
-        execute "CREATE VIEW #{name} AS #{sql_definition};"
-      end
-
-      def self.create_materialized_view(name, sql_definition)
+      # Creates a materialized view in the database
+      #
+      # @param name The name of the materialized view to create
+      # @param sql_definition The SQL schema that defines the materialized view.
+      # @return [void]
+      def create_materialized_view(name, sql_definition)
         execute "CREATE MATERIALIZED VIEW #{name} AS #{sql_definition};"
       end
 
-      def self.drop_view(name)
-        execute "DROP VIEW #{name};"
-      end
-
-      def self.drop_materialized_view(name)
+      # Drops a materialized view in the database
+      #
+      # @param name The name of the materialized view to drop.
+      # @return [void]
+      def drop_materialized_view(name)
         execute "DROP MATERIALIZED VIEW #{name};"
       end
 
-      def self.refresh_materialized_view(name)
+      # Refreshes a materialized view from its SQL schema.
+      #
+      # @param name The name of the materialized view to refresh..
+      # @return [void]
+      def refresh_materialized_view(name)
         execute "REFRESH MATERIALIZED VIEW #{name};"
-      end
-
-      private
-
-      def self.execute(sql, base = ActiveRecord::Base)
-        base.connection.execute sql
       end
     end
   end
