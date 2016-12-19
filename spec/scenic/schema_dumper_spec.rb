ae53ea59e3484cf75e76b160b5ca8c0e36b65c76
Dorian Marié <dorian@doma.io>
2016-12-19 14:22:19 -0800

    Sort views by name when dumped to the schema
    - Makes for more consistent schema.rb generation
    - Makes it easier to spot the changes / diff from the previous version

--- spec/scenic/schema_dumper_spec.rb
+++ spec/scenic/schema_dumper_spec.rb
@@ -21,6 +21,35 @@ describe Scenic::SchemaDumper, :db do
     expect(Search.first.haystack).to eq "needle"
   end
 
+  it "sorts the views by name when dumped" do
+    view_definition = "SELECT 'needle'::text AS haystack"
+    Search.connection.create_view :searches_b, sql_definition: view_definition
+    Search.connection.create_view :searches_c, sql_definition: view_definition
+    Search.connection.create_view :searches_a, sql_definition: view_definition
+    stream = StringIO.new
+
+    ActiveRecord::SchemaDumper.dump(Search.connection, stream)
+
+    output = stream.string
+    expect(output).to include "create_view :searches_a"
+    expect(output).to include "create_view :searches_b"
+    expect(output).to include "create_view :searches_c"
+    expect(output).to include view_definition
+
+    a_index = output.lines.find_index { |l| l.include?("searches_a") }
+    b_index = output.lines.find_index { |l| l.include?("searches_b") }
+    c_index = output.lines.find_index { |l| l.include?("searches_c") }
+
+    expect(a_index).to be < b_index
+    expect(b_index).to be < c_index
+
+    Search.connection.drop_view :searches_a
+    Search.connection.drop_view :searches_b
+    Search.connection.drop_view :searches_c
+
+    silence_stream(STDOUT) { eval(output) }
+  end
+
   context "with views in non public schemas" do
     it "dumps a create_view including namespace for a view in the database" do
       view_definition = "SELECT 'needle'::text AS haystack"
