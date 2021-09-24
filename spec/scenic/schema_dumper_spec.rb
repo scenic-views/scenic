c437ffc300fe3728ea1f7b2f9c17c92396b09356
Mykhaylo Sorochan <mykhaylo.sorochan@toptal.com>
2021-09-24 17:22:55 +0400

    Add topological sorting for dumped views using TSort

--- spec/scenic/schema_dumper_spec.rb
+++ spec/scenic/schema_dumper_spec.rb
@@ -7,14 +7,15 @@ class SearchInAHaystack < ActiveRecord::Base
 end
 
 describe Scenic::SchemaDumper, :db do
+  let(:output) do
+    stream = StringIO.new
+    ActiveRecord::SchemaDumper.dump(Search.connection, stream)
+    stream.string
+  end
+
   it "dumps a create_view for a view in the database" do
     view_definition = "SELECT 'needle'::text AS haystack"
     Search.connection.create_view :searches, sql_definition: view_definition
-    stream = StringIO.new
-
-    ActiveRecord::SchemaDumper.dump(Search.connection, stream)
-
-    output = stream.string
 
     expect(output).to include 'create_view "searches", sql_definition: <<-SQL'
     expect(output).to include view_definition
@@ -45,11 +46,6 @@ describe Scenic::SchemaDumper, :db do
   it "dumps a create_view for a materialized view in the database" do
     view_definition = "SELECT 'needle'::text AS haystack"
     Search.connection.create_view :searches, materialized: true, sql_definition: view_definition
-    stream = StringIO.new
-
-    ActiveRecord::SchemaDumper.dump(Search.connection, stream)
-
-    output = stream.string
 
     expect(output).to include 'create_view "searches", materialized: true, sql_definition: <<-SQL'
     expect(output).to include view_definition
@@ -57,21 +53,13 @@ describe Scenic::SchemaDumper, :db do
 
   context "with views in non public schemas" do
     it "dumps a create_view including namespace for a view in the database" do
-      view_definition = "SELECT 'needle'::text AS haystack"
-      Search.connection.execute "CREATE SCHEMA scenic; SET search_path TO scenic, public"
-      Search.connection.create_view :"scenic.searches", sql_definition: view_definition
-      stream = StringIO.new
+      Search.connection.create_view :"scenic.searches",
+        sql_definition: "SELECT 'needle'::text AS haystack"
 
-      ActiveRecord::SchemaDumper.dump(Search.connection, stream)
-
-      output = stream.string
       expect(output).to include 'create_view "scenic.searches",'
-
-      Search.connection.drop_view :"scenic.searches"
     end
 
     it "sorts dependency order when views exist in a non-public schema" do
-      Search.connection.execute("CREATE SCHEMA IF NOT EXISTS scenic; SET search_path TO public, scenic")
       Search.connection.execute("CREATE VIEW scenic.apples AS SELECT 1;")
       Search.connection.execute("CREATE VIEW scenic.bananas AS SELECT 2;")
       Search.connection.execute("CREATE OR REPLACE VIEW scenic.apples AS SELECT * FROM scenic.bananas;")
@@ -83,7 +71,18 @@ describe Scenic::SchemaDumper, :db do
       end
       expect(views).to eq(%w[scenic.bananas scenic.apples])
 
-      Search.connection.execute("DROP SCHEMA IF EXISTS scenic CASCADE; SET search_path TO public")
+    end
+
+    before(:each) do
+      Search.connection.execute(
+        "CREATE SCHEMA IF NOT EXISTS scenic; SET search_path TO public, scenic"
+      )
+    end
+
+    after(:each) do
+      Search.connection.execute(
+        "DROP SCHEMA IF EXISTS scenic CASCADE; SET search_path TO public"
+      )
     end
   end
 
@@ -104,11 +103,6 @@ describe Scenic::SchemaDumper, :db do
   it "ignores tables internal to Rails" do
     view_definition = "SELECT 'needle'::text AS haystack"
     Search.connection.create_view :searches, sql_definition: view_definition
-    stream = StringIO.new
-
-    ActiveRecord::SchemaDumper.dump(Search.connection, stream)
-
-    output = stream.string
 
     expect(output).to include 'create_view "searches"'
     expect(output).not_to include "pg_stat_statements_info"
@@ -119,11 +113,7 @@ describe Scenic::SchemaDumper, :db do
     it "dumps a create_view for a view in the database" do
       view_definition = "SELECT 'needle'::text AS haystack"
       Search.connection.create_view '"search in a haystack"', sql_definition: view_definition
-      stream = StringIO.new
 
-      ActiveRecord::SchemaDumper.dump(Search.connection, stream)
-
-      output = stream.string
       expect(output).to include 'create_view "\"search in a haystack\"",'
       expect(output).to include view_definition
 
@@ -139,15 +129,11 @@ describe Scenic::SchemaDumper, :db do
     it "dumps a create_view for a view in the database" do
       view_definition = "SELECT 'needle'::text AS haystack"
       Search.connection.execute(
-        "CREATE SCHEMA scenic; SET search_path TO scenic, public"
+        "CREATE SCHEMA scenic; SET search_path TO scenic, public",
       )
       Search.connection.create_view 'scenic."search in a haystack"',
         sql_definition: view_definition
-      stream = StringIO.new
 
-      ActiveRecord::SchemaDumper.dump(Search.connection, stream)
-
-      output = stream.string
       expect(output).to include 'create_view "scenic.\"search in a haystack\"",'
       expect(output).to include view_definition
 
@@ -156,6 +142,108 @@ describe Scenic::SchemaDumper, :db do
       silence_stream($stdout) { eval(output) } # standard:disable Security/Eval
 
       expect(SearchInAHaystack.take.haystack).to eq "needle"
+    end
+  end
+
+  context "with views ordered by name" do
+    it "sorts views without dependencies" do
+      Search.connection.create_view "cucumber_needles",
+        sql_definition: "SELECT 'kukumbas'::text as needle"
+      Search.connection.create_view "vip_needles",
+        sql_definition: "SELECT 'vip'::text as needle"
+      Search.connection.create_view "none_needles",
+        sql_definition: "SELECT 'none_needles'::text as needle"
+
+      # Same here, no dependencies among existing views, all views are sorted
+      sorted_views = %w[cucumber_needles vip_needles none_needles].sort
+
+      expect(views_in_output_order).to eq(sorted_views)
+    end
+
+    it "sorts according to dependencies" do
+      Search.connection.create_table(:tasks) { |t| t.integer :performer_id }
+      Search.connection.create_table(:notes) { |t|
+        t.text :title
+        t.integer :author_id
+      }
+      Search.connection.create_table(:users) { |t| t.text :nickname }
+      Search.connection.create_table(:roles) { |t|
+        t.text :name
+        t.integer :user_id
+      }
+
+      Search.connection.create_view "recent_tasks", sql_definition: <<-SQL
+        SELECT id, performer_id FROM tasks WHERE id > 42
+      SQL
+      Search.connection.create_view "old_roles", sql_definition: <<-SQL
+        SELECT id, name FROM roles WHERE id > 56
+      SQL
+      Search.connection.create_view "nirvana_notes", sql_definition: <<-SQL
+        SELECT notes.id, notes.title, notes.author_id, old_roles.name
+        FROM notes
+        JOIN old_roles ON notes.author_id = old_roles.id
+      SQL
+      Search.connection.create_view "angry_zombies", sql_definition: <<-SQL
+        SELECT
+          users.nickname,
+          recent_tasks.id AS task_id,
+          nirvana_notes.title AS talk
+        FROM users
+        JOIN roles ON roles.user_id = users.id
+        JOIN nirvana_notes ON nirvana_notes.author_id = roles.id
+        JOIN recent_tasks ON recent_tasks.performer_id = roles.id
+      SQL
+      Search.connection.create_view "doctor_zombies", sql_definition: <<-SQL
+        SELECT id FROM old_roles WHERE name LIKE '%Dr%'
+      SQL
+      Search.connection.create_view "xenomorphs", sql_definition: <<-SQL
+        SELECT id, name FROM roles WHERE name = 'xeno'
+      SQL
+      Search.connection.create_view "important_messages", sql_definition: <<-SQL
+        SELECT id, title FROM notes WHERE title LIKE '%IMPORTANT%'
+      SQL
+
+      # Converted with https://github.com/ggerganov/dot-to-ascii
+      # digraph {
+      #   rankdir = "RL";
+      #   xenomorphs;
+      #   important_messages;
+      #   doctor_zombies -> old_roles;
+      #   nirvana_notes -> old_roles;
+      #   angry_zombies -> nirvana_notes;
+      #   angry_zombies -> recent_tasks;
+      # }
+      #
+      #                                                                +--------------------+
+      #                                                                |    recent_tasks    |
+      #                                                                +--------------------+
+      #                                                                  ^
+      #                                                                  |
+      #                                                                  |
+      # +----------------+     +-----------+     +---------------+     +--------------------+
+      # | doctor_zombies | --> | old_roles | <-- | nirvana_notes | <-- |    angry_zombies   |
+      # +----------------+     +-----------+     +---------------+     +--------------------+
+      #                                                                +--------------------+
+      #                                                                | important_messages |
+      #                                                                +--------------------+
+      #                                                                +--------------------+
+      #                                                                |     xenomorphs     |
+      #                                                                +--------------------+
+      expect(views_in_output_order).to eq(%w[
+        old_roles
+        nirvana_notes
+        recent_tasks
+        angry_zombies
+        doctor_zombies
+        important_messages
+        xenomorphs
+      ])
+    end
+
+    def views_in_output_order
+      output.lines.grep(/create_view/).map do |view_line|
+        view_line.match('create_view "(?<name>.*)"')[:name]
+      end
     end
   end
 end
