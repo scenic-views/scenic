ae3d608901d5671263ff858cdd9d31c9d160b247
Mykhaylo Sorochan <mykhaylo.sorochan@toptal.com>
2021-09-24 17:22:55 +0400

    Add topological sorting for dumped views using TSort

--- lib/scenic/schema_dumper.rb
+++ lib/scenic/schema_dumper.rb
@@ -3,6 +3,34 @@ require "rails"
 module Scenic
   # @api private
   module SchemaDumper
+    # A hash to do topological sort
+    class TSortableHash < Hash
+      include TSort
+
+      alias tsort_each_node each_key
+      def tsort_each_child(node, &block)
+        fetch(node).each(&block)
+      end
+    end
+
+    # Query for the dependencies between views
+    DEPENDENT_SQL = <<~SQL.freeze
+      SELECT distinct dependent_ns.nspname AS dependent_schema
+      , dependent_view.relname AS dependent_view
+      , source_ns.nspname AS source_schema
+      , source_table.relname AS source_table
+      FROM pg_depend
+      JOIN pg_rewrite ON pg_depend.objid = pg_rewrite.oid
+      JOIN pg_class as dependent_view ON pg_rewrite.ev_class = dependent_view.oid
+      JOIN pg_class as source_table ON pg_depend.refobjid = source_table.oid
+      JOIN pg_namespace dependent_ns ON dependent_ns.oid = dependent_view.relnamespace
+      JOIN pg_namespace source_ns ON source_ns.oid = source_table.relnamespace
+      WHERE dependent_ns.nspname = ANY (current_schemas(false)) AND source_ns.nspname = ANY (current_schemas(false))
+      AND source_table.relname != dependent_view.relname
+      AND source_table.relkind IN ('m', 'v') AND dependent_view.relkind IN ('m', 'v')
+      ORDER BY dependent_view.relname;
+    SQL
+
     def tables(stream)
       super
       views(stream)
@@ -22,9 +50,35 @@ module Scenic
     private
 
     def dumpable_views_in_database
-      @dumpable_views_in_database ||= Scenic.database.views.reject do |view|
+      return @ordered_dumpable_views_in_database if @ordered_dumpable_views_in_database
+
+      existing_views = Scenic.database.views.reject do |view|
         ignored?(view.name)
       end
+
+      @ordered_dumpable_views_in_database = tsorted_views(existing_views.map(&:name)).map do |view_name|
+        existing_views.find { |ev| ev.name == view_name }
+      end.compact
+    end
+
+    # When dumping the views, their order must be topologically sorted to take into account dependencies
+    def tsorted_views(views_names)
+      views_hash = TSortableHash.new
+
+      ::Scenic.database.execute(DEPENDENT_SQL).each do |relation|
+        source_v = relation["source_table"]
+        dependent = relation["dependent_view"]
+        views_hash[dependent] ||= []
+        views_hash[source_v] ||= []
+        views_hash[dependent] << source_v
+        views_names.delete(source_v)
+        views_names.delete(dependent)
+      end
+
+      # after dependencies, there might be some views left that don't have any dependencies
+      views_names.sort.each { |v| views_hash[v] = [] }
+
+      views_hash.tsort
     end
 
     unless ActiveRecord::SchemaDumper.private_instance_methods(false).include?(:ignored?)
